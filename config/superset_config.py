4b98831f89fc43c94b13ec5db7fe642cec97ef24
Gandhar <gandharjoshi47@gmail.com>
2025-12-19 11:51:02 +0530

    fix roll back appliaction root with correct file

--- config/superset_config.py
+++ config/superset_config.py
@@ -270,7 +270,7 @@ IS_RENDER = os.getenv("RENDER", "").lower() == "true"
 
 if IS_RENDER:
     # Tell Superset it lives under /superset (Render only)
-    # APPLICATION_ROOT = "/superset"
+    APPLICATION_ROOT = "/superset"
 
     # Generate correct redirects and absolute URLs
     SUPERSET_WEBSERVER_BASEURL = "https://gateway-dev-gwnu.onrender.com/superset"
